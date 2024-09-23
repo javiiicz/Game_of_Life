0e3b2f9bd5ffce10cca3929429ced01d9742de95
Javier <106848262+javiiicz@users.noreply.github.com>
2024-09-23 11:17:08 -0400

    Add Grid class in charge of drawing

---
+++ src/grid.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Javier Carrillo on 9/23/24.
+//
+
+#include "grid.h"
+
+#include "SFML/Graphics.hpp"
+
+Grid::Grid(int rows, int cols, int cell_size){
+    this->b = std::make_unique<Board>(rows, cols);
+    this->cols = cols;
+    this->rows = rows;
+    this->cell_size = cell_size;
+
+    this->b->fill_random(0.5f);
+}
+
+void Grid::draw(sf::RenderWindow& window) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sf::RectangleShape cell(sf::Vector2f(cell_size, cell_size));
+            cell.setPosition(j * cell_size, i * cell_size);
+
+            // Set color
+            if (this->b->get(i, j)) {
+                cell.setFillColor(sf::Color::White);
+            } else {
+                cell.setFillColor(sf::Color::Black);
+            }
+
+            cell.setOutlineThickness(1);
+            cell.setOutlineColor(sf::Color::Black);
+            window.draw(cell);
+        }
+    }
+}
---
+++ src/grid.h
@@ -0,0 +1,38 @@
+//
+// Created by Javier Carrillo on 9/23/24.
+//
+
+#ifndef GRID_H
+#define GRID_H
+#include "board.h"
+#include "SFML/Graphics/RenderWindow.hpp"
+#include <memory>
+
+
+// Grid class for drawing on canvas
+class Grid {
+    std::unique_ptr<Board> b;
+    int rows;
+    int cols;
+    int cell_size;
+public:
+    /**
+     *  Constructs a grid object with a Board of the same size
+     * @param rows # of rows
+     * @param cols # of columns
+     * @param cell_size size of rendered cells
+     */
+    Grid(int rows, int cols, int cell_size);
+
+    /**
+     * Draws the current state of the board to the window
+     * @param window Window obj
+     */
+    void draw(sf::RenderWindow& window);
+
+
+};
+
+
+
+#endif //GRID_H
