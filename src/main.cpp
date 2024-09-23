49e506062186c538bf57aa685c6919f460579664
Javier <106848262+javiiicz@users.noreply.github.com>
2024-09-23 13:51:14 -0400

    Start / Stop / Step Functionality

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,15 @@ int main() {
     const int COLS = 200;
     const int CELL_SIZE = 5;
     const int MARGIN = 50;
+    const float PROB = 0.3f;
 
-    Grid g(ROWS, COLS, CELL_SIZE, MARGIN);
+    bool isPlaying = false;
+
+    Grid g(ROWS, COLS, CELL_SIZE, MARGIN, PROB);
 
     sf::RenderWindow window(sf::VideoMode(COLS * CELL_SIZE + 2*  MARGIN, ROWS * CELL_SIZE + 2 * MARGIN), "Game of Life");
 
-    window.setFramerateLimit(144);
+    window.setFramerateLimit(60);
 
     while (window.isOpen())
     {
@@ -22,10 +25,27 @@ int main() {
             {
                 window.close();
             }
+
+            // Start / Stop with spacebar
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Space) {
+                    isPlaying = !isPlaying;
+                }
+            }
+
+            // Step with right arrow
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Right) {
+                    g.advance();
+                }
+            }
         }
 
         window.clear();
         g.draw(window);
         window.display();
+        if (isPlaying) {
+            g.advance();
+        }
     }
 }
